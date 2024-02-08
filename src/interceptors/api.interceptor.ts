f1fa7e7f37ff7ff34a0046326f231668fd1de2f3
Inma Caleron <inmacale@gmail.com>
2024-02-08 14:06:45 +0100

    subida andres

--- src/interceptors/api.interceptor.ts
+++ src/interceptors/api.interceptor.ts
@@ -1,31 +1,38 @@
 // api.interceptor.ts
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable, map } from 'rxjs';
 
 @Injectable()
 export class ApiInterceptor implements HttpInterceptor {
 
+    urls: { path: string; file: string }[] = [
+        { path: 'data', file: 'data' }
+    ]
+
     constructor(private http: HttpClient) { }
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        const apiUrl = 'https://api.example.com/data';
+        //un if con enviroment este a true
 
-        if (request.url === apiUrl) {
+        this.urls.forEach(url => {
+            if (request.url.includes(url.path)) {
+                return this.http.get<any>('assets/mocks/' + url.file + '.json').pipe(
+                    map((data) => {
+                        const response = new HttpResponse({
+                            body: data,
+                            status: 200
+                        });
+                        console.log(response)
+                        return response;
+                    })
+                );
+            } else {
 
-            return this.http.get<any>('assets/mocks/data.json').pipe(
+                return next.handle(request);
+            }
+        });
 
-                map((data) => {
-                    const response = new HttpResponse({
-                        body: data,
-                        status: 200
-                    });
-                    return response;
-                })
-            );
-        } else {
 
-            return next.handle(request);
-        }
     }
 }
